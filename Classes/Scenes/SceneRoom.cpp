99c1858e57d8d83a760c26ff20268cd014377f7e
ZhenlyChen <zhenlychen@foxmail.com>
2018-07-05 15:52:53 +0800

    new

---
+++ Classes/Scenes/SceneRoom.cpp
@@ -0,0 +1,28 @@
+﻿#include "SceneRoom.h"
+
+Scene* SceneRoom::createScene()
+{
+    return SceneRoom::create();
+}
+
+bool SceneRoom::init()
+{
+    if (!Scene::init())
+    {
+        return false;
+    }
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+
+    return true;
+}
+
+void SceneRoom::updateLayer()
+{
+}
+
+void SceneRoom::updateScene()
+{
+
+}
--- Classes/Scenes/SceneRoom.h
+++ Classes/Scenes/SceneRoom.h
@@ -1,1 +1,22 @@
-#pragma once
+﻿#ifndef __SCENE_ROOM_H__
+#define __SCENE_ROOM_H__
+
+#include "LayerBase.h"
+#include "SceneBase.h"
+
+class SceneRoom final : public SceneBase
+{
+public:
+    static Scene* createScene();
+
+    virtual bool init() override;
+    virtual void updateLayer() override;
+    virtual void updateScene() override;
+
+    CREATE_FUNC(SceneRoom);
+private:
+
+    LayerBase * layerRoom;
+};
+
+#endif //__SCENE_ROOM_H__s

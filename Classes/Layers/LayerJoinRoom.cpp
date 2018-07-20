06f658ed63a5acbcefc1d4e5b8c771fc011f8054
MegaShow <MegaXiu@outlook.com>
2018-07-21 00:55:52 +0800

    :construction: Add LayerJoinRoomCard

--- Classes/Layers/LayerJoinRoom.cpp
+++ Classes/Layers/LayerJoinRoom.cpp
@@ -1,5 +1,6 @@
 ﻿#include "Helpers.h"
 #include "LayerJoinRoom.h"
+#include "LayerJoinRoomCard.h"
 #include "ServiceAPI.h"
 
 LayerBase* LayerJoinRoom::createLayer()
@@ -21,7 +22,7 @@ bool LayerJoinRoom::init()
     menu->setPosition(origin);
 
     // 页码显示
-    auto currentPage = Label::createWithTTF("0 /", "Fonts/arial.ttf", 20);
+    auto currentPage = Label::createWithTTF("0  /", "Fonts/arial.ttf", 20);
     auto maxPage = Label::createWithTTF("0", "Fonts/arial.ttf", 20);
 
     currentPage->setPosition(Vec2(visibleSize.width / 2 - 10, visibleSize.height / 2 - 300));
@@ -52,6 +53,7 @@ bool LayerJoinRoom::init()
     this->addChild(maxPage, 1);
     this->addChild(background, 1);
 
+    pageNum = 1;
 
     // 定时扫描
     schedule(schedule_selector(LayerJoinRoom::getRoomList), 2.0f, kRepeatForever, 0);
@@ -69,83 +71,20 @@ void LayerJoinRoom::getRoomList(float dt)
 {
     // todo 获取房间列表API
     if (this->getActive() == false) return;
-    auto d = Singleton<ServiceAPI>::GetInstance()->GetRoomsList(1, 5);
+    auto d = Singleton<ServiceAPI>::GetInstance()->GetRoomsList(pageNum, 5);
     if (!d.HasParseError() && d.IsObject() && d.HasMember("status"))
     {
         if (strcmp(d["status"].GetString(), "success") == 0)
         {
-            auto padding = 20;
-            auto itemwidth = 400.0f;
-            auto itemheight = 100.0f;
-            auto mapimagewidth = 70.0f;
-            for (auto i = 0; i < 5; i++)
+            for (auto &room : d["rooms"].GetArray())
             {
-                auto itembackground = Sprite::create("Graphics/System/BtnBack.png");
-                itembackground->setContentSize(Size::Size(itemwidth, itemheight));
-                itembackground->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2 + 220 - i *itemheight - padding));
-
-                auto mapimage = Sprite::create("Graphics/Pictures/background.png");
-                mapimage->setContentSize(Size::Size(mapimagewidth, mapimagewidth));
-                mapimage->setPosition(Vec2(visibleSize.width / 2 - 150, visibleSize.height / 2 + 220 - i * itemheight - padding));
-
-                auto roomname = Label::createWithTTF("Room Name: ", "Fonts/arial.ttf", 15);
-                roomname->setColor(Color3B::BLACK);
-                roomname->setAnchorPoint(Vec2(0, 0.5));
-                roomname->setPosition(Vec2(visibleSize.width / 2 - 110, visibleSize.height / 2 + 240 - i * itemheight - padding));
-
-                auto owner = Label::createWithTTF("Owner: ", "Fonts/arial.ttf", 15);
-                owner->setColor(Color3B::BLACK);
-                owner->setAnchorPoint(Vec2(0, 0.5));
-                owner->setPosition(Vec2(visibleSize.width / 2 - 110, visibleSize.height / 2 + 220 - i * itemheight - padding));
-
-                auto mapname = Label::createWithTTF("Map: ", "Fonts/arial.ttf", 15);
-                mapname->setColor(Color3B::BLACK);
-                mapname->setAnchorPoint(Vec2(0, 0.5));
-                mapname->setPosition(Vec2(visibleSize.width / 2 - 110, visibleSize.height / 2 + 200 - i * itemheight - padding));
-
-                // 是否有密码
-                if (true) {
-                    auto lock = Sprite::create("Graphics/System/lock.jpg");
-                    lock->setContentSize(Size::Size(25, 25));
-                    lock->setPosition(Vec2(visibleSize.width / 2 + 90, visibleSize.height / 2 + 240 - i * itemheight - padding));
-                    this->addChild(lock, 3);
-                }
-
-                // 房间状态 waiting / playing
-                auto status = Label::createWithTTF("", "Fonts/arial.ttf", 15);
-                status->setAnchorPoint(Vec2(1, 0.5));
-                if (true)
-                {
-                    status->setString("waiting");
-                    status->setColor(Color3B::GREEN);
-                }
-                else
-                {
-                    status->setString("playing");
-                    status->setColor(Color3B::YELLOW);
-                }
-                status->setPosition(Vec2(visibleSize.width / 2 + 160, visibleSize.height / 2 + 240 - i * itemheight - padding));
-
-                // 游戏模式 random / optional
-                auto mode = Label::createWithTTF("Mode: optional", "Fonts/arial.ttf", 15);
-                mode->setAnchorPoint(Vec2(1, 0.5));
-                mode->setColor(Color3B::BLACK);
-                mode->setPosition(Vec2(visibleSize.width / 2 + 160, visibleSize.height / 2 + 220 - i * itemheight - padding));
-
-                auto count = Label::createWithTTF("10/10", "Fonts/arial.ttf", 15);
-                count->setAnchorPoint(Vec2(1, 0.5));
-                count->setColor(Color3B::BLACK);
-                count->setPosition(Vec2(visibleSize.width / 2 + 160, visibleSize.height / 2 + 200 - i * itemheight - padding));
-
-                this->addChild(status, 3);
-                this->addChild(roomname, 3);
-                this->addChild(owner, 3);
-                this->addChild(mapname, 3);
-                this->addChild(mode, 3);
-                this->addChild(count, 3);
-                this->addChild(mapimage, 3);
-                this->addChild(itembackground, 2);
-                
+                ModelRoom room;
+            }
+            for (auto i = 1; i <= 5; i++)
+            {
+                auto card = LayerJoinRoomCard::createLayerWithRoom(ModelRoom());
+                card->setPosition(Vec2((visibleSize.width - card->getContentSize().width) / 2, visibleSize.height - 120 - i * card->getContentSize().height));
+                this->addChild(card, 1);
             }
             // 根据数据生成房间列表
         }
---
+++ Classes/Models/ModelRoom.h
@@ -0,0 +1,24 @@
+﻿#ifndef __MODEL_ROOM_H__
+#define __MODEL_ROOM_H__
+
+#include <string>
+
+using namespace std;
+
+class ModelRoom final
+{
+public:
+    int Id;
+    string OwnId;
+    int Port;
+    string Title;
+    bool IsRandom;
+    string GameMap;
+    int MaxPlayer;
+    string Mode;
+    string Password;
+    bool Playing;
+    // todo Players
+};
+
+#endif // __MODEL_ROOM_H__

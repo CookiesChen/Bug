c088a7a5276caca5657749185655641d15e29100
CookiesChen <1109349604@qq.com>
2018-07-21 14:36:55 +0800

    layer

---
+++ Classes/Layers/LayerMap.h
@@ -0,0 +1,14 @@
+﻿#ifndef _LAYER_MAP_H__
+#define _LAYER_MAP_H__
+
+#include "LayerBase.h"
+
+class LayerMap final : public LayerBase
+{
+public:
+    void movemap();
+private:
+    TMXTiledMap * map;
+};
+
+#endif // !_LAYER_PLAYER_H
---
+++ Classes/Layers/LayerPlayer.h
@@ -0,0 +1,14 @@
+﻿#ifndef _LAYER_PLAYER_H__
+#define _LAYER_PLAYER_H__
+
+#include "LayerBase.h"
+
+class LayerPlayer final: public LayerBase
+{
+public:
+
+private:
+    TMXTiledMap * map;
+};
+
+#endif // !_LAYER_PLAYER_H

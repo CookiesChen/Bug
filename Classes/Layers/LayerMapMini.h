f45a52c73f7e9728b14570ca64aac808e6f94c9a
ZhenlyChen <zhenlychen@foxmail.com>
2018-07-22 15:19:18 +0800

    add

---
+++ Classes/Layers/LayerMapMini.h
@@ -0,0 +1,21 @@
+﻿#ifndef _LAYER_MAP_MINI_H__
+#define _LAYER_MAP_MINI_H__
+
+#include "LayerBase.h"
+
+class LayerMap final : public LayerBase
+{
+public:
+    static LayerBase* createLayer();
+
+    virtual bool init() override;
+
+    float maxWidth;
+    float maxHeight;
+    experimental::TMXTiledMap * map;
+
+    CREATE_FUNC(LayerMap);
+
+};
+
+#endif // !_LAYER_MAP_MINI_H__
